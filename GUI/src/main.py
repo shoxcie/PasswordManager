a4f38a725909e649af9fa8004fbd36f4b6ff2499
shoxcie <shoxcie@gmail.com>
2025-02-17 19:54:17 +0200

    [GUI] Add Login tab

--- GUI/src/main.py
+++ GUI/src/main.py
@@ -50,19 +50,22 @@ def app():
 	#-------------#
 	tab_file_search = [f for f in os.listdir(DIR_DOCS) if f.endswith('.shxc')]
 	tab_file_reason = ''
+	tab_login_reason = ''
 	if len(tab_file_search) == 0:
 		tab_file_reason = "  Database file not found, please select or create one"
 	elif len(tab_file_search) > 1:
 		tab_file_reason = "  Multiple database files found, please select one"
+	else:
+		tab_login_reason = os.path.join(DIR_DOCS, tab_file_search[0])
 	
 	#----------#
 	# Notebook #
 	#----------#
 	notebook = ttk.Notebook(root)
 	notebook.pack(expand=True, fill='both')
-	tab_file = my_gui.notebook.Tab(notebook, tab_file_reason, 'file.png')
+	tab_file = my_gui.notebook.Tab(notebook, tab_file_reason, 'file.png').hide()
 	tab_signup = my_gui.notebook.Tab(notebook, "  SignUp", 'key.png').hide()
-	tab_login = my_gui.notebook.Tab(notebook, "  Login", 'key.png').hide()
+	tab_login = my_gui.notebook.Tab(notebook, f" File \"{tab_login_reason}\"", 'key.png').hide()
 	tab_password = my_gui.notebook.Tab(notebook, "  Password", 'key.png').hide()
 	tab_database = my_gui.notebook.Tab(notebook, "  Database", 'list.png').hide()
 	tab_entry = my_gui.notebook.Tab(notebook, "  Entry", 'entry.png').hide()
@@ -163,7 +166,42 @@ def app():
 	#------------#
 	# Tab: Login #
 	#------------#
+	tab_login_show_bool = False
 	
+	def tab_login_onclick_show():
+		nonlocal tab_login_show_bool
+		tab_login_show_bool = not tab_login_show_bool
+		
+		if tab_login_onclick_show:
+			tab_login_entry.config(show='')
+		else:
+			tab_login_entry.config(show='*')
+	
+	def tab_login_onclick_confirm():
+		pass
+		# TODO: Decrypt the database file with tab_login_entry.get() password
+		#	if the password is correct, show main tabs
+		#	else, show error message
+	
+	if not tab_file_reason:
+		tab_login.show()
+	
+	ttk.Button(tab_login.frame, text="Toggle View", command=tab_login_onclick_show).pack(
+		padx=(WIN_MIN_WIDTH / 3), fill='x', pady=20
+	)
+	
+	tab_login_entry_frame = ttk.Frame(tab_login.frame)
+	tab_login_entry_frame.pack(
+		padx=(WIN_MIN_WIDTH / 6), fill='x', pady=0
+	)
+	
+	ttk.Label(tab_login_entry_frame, text="Password:").pack(anchor='w')
+	tab_login_entry = ttk.Entry(tab_login_entry_frame, show='*')
+	tab_login_entry.pack(fill='x')
+	
+	ttk.Button(tab_login.frame, text="Confim", command=tab_login_onclick_confirm).pack(
+		padx=(WIN_MIN_WIDTH / 3), fill='x', pady=30
+	)
 	
 	#---------------#
 	# Tab: Password #
